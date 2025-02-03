6e836fc36dcdf81c6140b92c0ba44fa0b215acd9
yasithrashan2003 <y.rashan22@gmail.com>
2025-02-03 18:09:10 +0530

    Remove hash from url on navbar click.

--- script-files/nav-bar-script.js
+++ script-files/nav-bar-script.js
@@ -2,3 +2,13 @@ function closeNavbar() {
     document.getElementById('check').checked = false;
 }
 
+function scrollToSection(event) {
+    event.preventDefault(); // Prevent default link behavior
+    const targetId = event.target.getAttribute("data-target");
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+        history.replaceState(null, null, " "); // Remove # from URL
+    }
+}
